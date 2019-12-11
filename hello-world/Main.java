e1059e86d6212979fc276ffbda5aa492e62e5d50
zainul88 <zainul88@users.noreply.github.com>
2019-12-11 15:30:01 +0530

    Update Main.java

--- hello-world/Main.java
+++ hello-world/Main.java
@@ -1,5 +1,10 @@
+import java.util.HashMap;
+
 public class Main {
-  public static void main(String[] args){
-    System.out.println("Hello "+ args[0]);
-  }
+	public static void main(String[] args) {
+		HashMap<Integer, String> map = new HashMap<>();
+		map.put(1, args[0]);
+		map.put(2, args[1]);
+		System.out.println(map.get(1) + " "+ map.get(2));
+	}
 }
