cc6e35cebe67aeb72d4337af67ef5d37cfc64203
zainul88 <zainul88@users.noreply.github.com>
2019-12-11 13:57:11 +0530

    Update Main.java

--- hello-world/Main.java
+++ hello-world/Main.java
@@ -1,5 +1,5 @@
 public class Main {
   public static void main(String[] args){
-    System.out.println(args[0]);
+    System.out.println("Hello.. this is the first testing for docker using Java");
   }
 }
